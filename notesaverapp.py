7da8352b5cd5c47fd6a04c297369f31c780a923d
RupeshBash <rpshbashyal@gmail.com>
2025-06-13 18:38:36 +0545

    This is a notesaverapp made to understand fundamentals of file handling

--- notesaverapp.py
+++ notesaverapp.py
@@ -1,6 +1,6 @@
 #note saver app
 
-note= input("Enter your note: ")
+note = input("Enter your note: ")
 
 #save the note to notes.txt(a there is append mode)
 with open("notes.txt", "a") as file:
@@ -10,7 +10,7 @@ print("Note saved successfully!")
 
 # Read and display all notes
 print("Here is all notes so far:")
-with open(notes.txt, "r") as file:
+with open("notes.txt", "r") as file:
     notes = file.readlines()
-    for index,line in enumerate(notes, start=1):
+    for index, line in enumerate(notes, start=1):
         print(f"{index}. {line.strip()}")
